39ef74ea88879917ce8b5c55f4db32cec8e257bf
barta4 <barta4@users.noreply.github.com>
2025-01-06 23:38:29 -0300

    Update MainActivity.java

--- app/src/main/java/com/nltechno/dolidroidpro/MainActivity.java
+++ app/src/main/java/com/nltechno/dolidroidpro/MainActivity.java
@@ -15,7 +15,26 @@
  * or see http://www.gnu.org/
  */
 package com.nltechno.dolidroidpro;
-
+import android.Manifest;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
